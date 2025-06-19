924da35adb58d8636a20f58a7da14ca2b0aeb9f5
abir943 <iu781721@gmail.com>
2025-06-19 11:45:21 +0600

    Delete cmds directory

--- cmds/tid.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "tid",
-    execute(api, event, args) {
-        const threadID = event.threadID;
-        api.sendMessage(`Your Thread ID is: ${threadID}`, event.threadID, event.messageID);
-    }
-};
